67811fe001e95c5834c192187ef731d1130d714f
Sophia (Fuhui) Lin <fuhuilin@Fuhuis-MacBook-Pro.local>
2020-04-24 15:51:54 -0500

    changes

--- fastsc/coloring/google_like.py
+++ fastsc/coloring/google_like.py
@@ -1,6 +1,157 @@
 from fastsc.util import get_connectivity_graph, get_aug_line_graph, get_map_circuit, get_layer_circuits, get_nearest_neighbor_coupling_list
 import networkx as nx
 
+def tiling_layer(layers, tilings, pattern_offset, verbose):
+    if (len(layers) < 1):
+        print("Too few layers for tiling_layer")
+        sys.exit()
+
+    new_qregs = layers[0].qregs
+    new_cregs = layers[0].cregs
+    if len(new_qregs) > 1:
+        print("Multiple q registers.")
+    if len(new_cregs) == 0:
+        new_circuit = QuantumCircuit(new_qregs[0])
+    elif len(new_cregs) == 1:
+        new_circuit = QuantumCircuit(new_qregs[0], new_cregs[0])
+    else:
+        print("Multiple c registers.")
+        new_circuit = QuantumCircuit(new_qregs[0])
+
+    num_layers = len(layers)
+    #print("nl %d" % num_layers)
+    idx = 1
+    pattern_idx = 0
+    pending_g = []
+    patterns = []
+    for g in layers[0].data:
+        pending_g.append(g)
+    while len(pending_g) > 0 or idx < num_layers:
+        #print("idx %d" % idx)
+        #print(coupling)
+        active_q = [] # active qubits in this layer
+        next_pd_g = [] # gates/instructions
+        two_q_layer = False
+        two_q_tiling_delay = [] # 2q gates delayed due to tiling
+        for inst, qargs, cargs in pending_g:
+            if len(qargs) == 2:
+                # two-qubit gates
+                q1 = qargs[0].index
+                q2 = qargs[1].index
+                #print("Looking: (%d,%d)" % (q1, q2))
+                #print("Active: ", active_q)
+                conf = False
+                if (q1 in active_q or q2 in active_q):
+                    conf = True
+                    next_pd_g.append((inst, qargs, cargs))
+                    if (verbose == 0):
+                        print("Delay gate (%d, %d)" % (q1,q2))
+                #for (n1, n2) in coupling:
+                #    if (q1 == n1 and n2 in active_q) or (q2 == n1 and n2 in active_q) or (q1 == n2 and n1 in active_q) or (q2 == n2 and n1 in active_q):
+                #        conf = True
+                #        if verbose==0:
+                #            print("Delay gate (%d, %d)" % (q1,q2))
+                #        break
+                if not conf:
+                    if (not((q1,q2) in tilings[(pattern_offset+pattern_idx) % 8] or (q2,q1) in tilings[(pattern_offset+pattern_idx) % 8])):
+                        conf = True
+                        two_q_tiling_delay.append((inst, qargs, cargs))
+                        if verbose==0:
+                            print("Delay gate (%d, %d) due to tiling" % (q1,q2))
+
+                if not conf:
+                    two_q_layer = True
+                    active_q.append(q1)
+                    active_q.append(q2)
+                    new_circuit.data.append((inst, qargs, cargs))
+
+            elif len(qargs) == 1:
+                # single-qubit gates
+                q1 = qargs[0].index
+                conf = False
+                if (q1 in active_q):
+                    conf = True
+                    if (verbose == 0):
+                        print("Delay gate %d" % q1)
+                if not conf:
+                    active_q.append(q1)
+                    new_circuit.data.append((inst, qargs, cargs))
+                else:
+                    next_pd_g.append((inst, qargs, cargs))
+            else:
+                # Multi-qubit gates; Barrier?
+                numq = len(qargs)
+                conf = False
+                for qii in range(numq):
+                    qi = qargs[qii].index
+                    if (qi in active_q):
+                        conf = True
+                if not conf:
+                    active_q.append(qi)
+                    new_circuit.data.append((inst, qargs, cargs))
+                else:
+                    next_pd_g.append((inst, qargs, cargs))
+
+        if two_q_layer == False and len(two_q_tiling_delay) > 0:
+            two_q_possible = []
+            for inst, qargs, cargs in two_q_tiling_delay:
+                q1 = qargs[0].index
+                q2 = qargs[1].index
+                if (q1 in active_q or q2 in active_q):
+                    next_pd_g.append((inst, qargs, cargs))
+                else:
+                    two_q_possible.append((inst, qargs, cargs))
+            if len(two_q_possible) > 0:
+                two_q_layer = True
+                inst, qargs, cargs = two_q_possible[0]
+                q1 = qargs[0].index
+                q2 = qargs[1].index
+                if (q1,q2) in tilings[pattern_offset % 8] or (q2,q1) in tilings[pattern_offset % 8]:
+                    pattern_idx = 0
+                elif (q1,q2) in tilings[(pattern_offset+1) % 8] or (q2,q1) in tilings[(pattern_offset+1) % 8]:
+                    pattern_idx = 1
+                elif (q1,q2) in tilings[(pattern_offset+2) % 8] or (q2,q1) in tilings[(pattern_offset+2) % 8]:
+                    pattern_idx = 2
+                elif (q1,q2) in tilings[(pattern_offset+3) % 8] or (q2,q1) in tilings[(pattern_offset+3) % 8]:
+                    pattern_idx = 3
+                elif (q1,q2) in tilings[(pattern_offset+4) % 8] or (q2,q1) in tilings[(pattern_offset+4) % 8]:
+                    pattern_idx = 4
+                elif (q1,q2) in tilings[(pattern_offset+5) % 8] or (q2,q1) in tilings[(pattern_offset+5) % 8]:
+                    pattern_idx = 5
+                elif (q1,q2) in tilings[(pattern_offset+6) % 8] or (q2,q1) in tilings[(pattern_offset+6) % 8]:
+                    pattern_idx = 6
+                elif (q1,q2) in tilings[(pattern_offset+7) % 8] or (q2,q1) in tilings[(pattern_offset+7) % 8]:
+                    pattern_idx = 7
+                for inst, qargs, cargs in two_q_possible:
+                    q1 = qargs[0].index
+                    q2 = qargs[1].index
+                    if (q1,q2) in tilings[(pattern_offset+pattern_idx) % 8] or (q2,q1) in tilings[(pattern_offset+pattern_idx) % 8]:
+                        if (q1 in active_q or q2 in active_q):
+                            next_pd_g.append((inst, qargs, cargs))
+                        else:
+                            new_circuit.data.append((inst, qargs, cargs))
+                            active_q.append(q1)
+                            active_q.append(q2)
+                    else:
+                        next_pd_g.append((inst, qargs, cargs))
+
+        pending_g = next_pd_g
+        if (len(pending_g) == 0 and idx < num_layers):
+            for g in layers[idx].data:
+                pending_g.append(g)
+            idx += 1
+        elif (idx < num_layers and not layers[idx].data[0][0].name == 'barrier'):
+            for g in layers[idx].data:
+                pending_g.append(g)
+            idx += 1
+        patterns.append((pattern_idx+pattern_offset) % 8)
+        new_circuit.barrier(new_qregs[0])
+        patterns.append((pattern_idx+pattern_offset) % 8)
+        if (two_q_layer):
+            pattern_idx += 1
+    return get_layer_circuits(new_circuit), (pattern_offset+pattern_idx) % 8, patterns
+
+
 def google_like(device, circuit, scheduler, d, decomp, verbose):
     freqsdata = []
     gatesdata = []
--- fastsc/coloring/util.py
+++ fastsc/coloring/util.py
@@ -1,3 +1,25 @@
 """
 util.py -  Utility functions
 """
+
+"""
+Input: A dictionary with (key, value) = (edge, int color)
+Return: A dictionary with (key, value) = (edge, int color), such that if a<b
+then more nodes are colored with a than b
+"""
+def relabel_coloring(int_coloring):
+    num_int = len(set(int_coloring.values()))
+    arr = [] # [(color, popularity) for each color]
+    for i in range(num_int):
+        num = len([k for k,v in int_coloring.items() if v == i])
+        arr.append((i,num))
+    arr.sort(key=lambda x:x[1]) # sort arr by popularity
+    new_coloring = {}
+    for i in range(num_int):
+        old_color = arr[num_int-i-1][0] # the new color is i
+        # collect the list of nodes with color = old_color
+        temp = [k for k,v in int_coloring.items() if v == old_color]
+        # color them with the new color
+        for k in temp:
+            new_coloring[k] = i
+    return new_coloring
