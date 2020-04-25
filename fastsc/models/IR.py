5834d789439adff975866f19f1c8fd129255c147
yongshanding <yongshanding@gmail.com>
2020-04-25 01:42:51 -0500

    adding IR class

---
+++ fastsc/models/IR.py
@@ -0,0 +1,48 @@
+"IR.py - IR (Intermediate Representation) of quantum circuit"
+
+from qiskit import Qubit, Instruction
+
+class Qbit(Qubit):
+    def __init__(self, q, omega01, omega12):
+        self.register = q.register
+        self.index = q.index
+        self.idle_freq = [omega01, omega12]
+
+class Inst(Instruction):
+    def __init__(self, ins, qargs, cargs, int_freq, gt):
+        self.name = ins.name
+        self.label = ins.label
+        self.qargs = qargs
+        self.cargs = cargs
+        self.int_freq = int_freq
+        self.gate_time = gt
+
+class IR(object):
+    def __init__(self, data=[], depth=0, qubits=[], width=0):
+        self.data = data
+        self.depth = depth
+        self.qubits = qubits
+        self.width = width
+    
+    def append_layer(self, layer):
+        self.data.append(layer)
+        self.depth += 1
+    
+    def append_layer_from_insts(self, insts):
+        active = []
+        gt = 0
+        freqs = [qubits[i].idle_freq for i in range(self.width)]
+        
+        for ins in insts:
+            int_freq = ins.int_freq
+            for q in ins.qargs:
+                if q in active:
+                    print("Warning: Qubit " + str(q.index) + " cannot be used twice in one time step.")
+                else:
+                   active.append(q)
+                freqs[q.index] = int_freq
+            if ins.gate_time >= gt:
+                gt = ins.gate_time
+        self.data.append((insts, freqs, gt))
+
+
--- fastsc/models/__init__.py
+++ fastsc/models/__init__.py
@@ -3,6 +3,8 @@
 #from .pulse import Pulse
 from .device import Device
 
+from .IR import Qbit, Inst, IR
+
 from .xtalknoise import swap_channel, leak_channel
 
-__all__ = ['Device', 'swap_channel', 'leak_channel']
+__all__ = ['Device', 'Qbit', 'Inst', 'IR', 'swap_channel', 'leak_channel']
